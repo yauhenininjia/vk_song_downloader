6d52018c6e61cc9982a23bab637871f49a5a90f8
yauhenininjia <thisislogiiiiin@gmail.com>
2016-01-18 01:11:46 +0300

    init app

---
+++ config/initializers/vkontakte_api.rb
@@ -0,0 +1,5 @@
+VkontakteApi.configure do |config|
+  config.app_id       = '5231584'      # ID приложения
+  config.app_secret   = '8VRntCrrCAuRS31YOCv3' # защищенный ключ
+  config.redirect_uri = 'http://localhost:3000/'
+end
