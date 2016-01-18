8d00f44deb2a6fd35e2758238c40fbecc87edcbf
yauhenininjia <thisislogiiiiin@gmail.com>
2016-01-18 05:55:08 +0300

    change redirect url

--- config/initializers/vkontakte_api.rb
+++ config/initializers/vkontakte_api.rb
@@ -1,6 +1,6 @@
 VkontakteApi.configure do |config|
   config.app_id       = '5231684'      # ID приложения
   config.app_secret   = 'Dq597gfPdBY0p0YZOlK6' # защищенный ключ
-  config.redirect_uri = 'vk-music-downloader.herokuapp.com/songs/index'
+  config.redirect_uri = 'https://vk-music-downloader.herokuapp.com'
   ###
 end
