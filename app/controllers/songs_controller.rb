79ca933676f3e3c7d4dc1a8a78d661f21d7cce4d
yauhenininjia <thisislogiiiiin@gmail.com>
2016-01-18 18:09:34 +0300

    add song downloading

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,30 @@
+require 'open-uri'
+
 class SongsController < ApplicationController
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  #after_action only: :download do
+  #  delete_songs
+  #end
 
   def index
     @vk = VkontakteApi::Client.new current_user.token
     @songs = @vk.audio.get
+    #binding.pry
+  end
+
+  def download
+    #binding.pry
+    uri = URI params[:url]
+    filename = "#{params[:filename]}.mp3"
+    file = File.new filename, 'wb'
+    file.write Net::HTTP.get uri
+    send_file file
+    puts 'data sent'
+  end
+
+  def delete_songs
+    puts 'delete songs'
+    #File.delete "#{params[:filename]}.mp3"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  get 'download' => 'songs#download'
   root 'songs#index'
 end
