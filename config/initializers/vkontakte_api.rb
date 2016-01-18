a84e5b4db762cd5c3bce65f8db4e805279ff6767
yauhenininjia <thisislogiiiiin@gmail.com>
2016-01-18 05:28:11 +0300

    fuck you heroku too

--- config/initializers/vkontakte_api.rb
+++ config/initializers/vkontakte_api.rb
@@ -1,5 +1,6 @@
 VkontakteApi.configure do |config|
   config.app_id       = '5231684'      # ID приложения
   config.app_secret   = 'Dq597gfPdBY0p0YZOlK6' # защищенный ключ
-  #config.redirect_uri = 'https://vk-music-downloader.herokuapp.com'
+  config.redirect_uri = 'https://vk-music-downloader.herokuapp.com'
+  ###
 end
