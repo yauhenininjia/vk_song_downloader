1b22150944c019bab099e12c217d5e8ede126ba3
yauhenininjia <thisislogiiiiin@gmail.com>
2016-01-18 05:52:42 +0300

    change redirect url

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,8 +4,7 @@ class SongsController < ApplicationController
 
   def index
     @vk = VkontakteApi::Client.new current_user.token
-    session[:state] = Digest::MD5.hexdigest(rand.to_s)
-    redirect_to VkontakteApi.authorization_url(scope: [:notify, :friends, :photos], state: session[:state])
-
+    
+    
   end
 end
--- config/initializers/vkontakte_api.rb
+++ config/initializers/vkontakte_api.rb
@@ -1,6 +1,6 @@
 VkontakteApi.configure do |config|
   config.app_id       = '5231684'      # ID приложения
   config.app_secret   = 'Dq597gfPdBY0p0YZOlK6' # защищенный ключ
-  config.redirect_uri = 'vk-music-downloader.herokuapp.com'
+  config.redirect_uri = 'vk-music-downloader.herokuapp.com/songs/index'
   ###
 end
