a05ec2f8aa843efde59acab0abaa7a426516dabd
yauhenininjia <thisislogiiiiin@gmail.com>
2016-01-18 04:44:08 +0300

    change vk config

--- config/initializers/vkontakte_api.rb
+++ config/initializers/vkontakte_api.rb
@@ -1,5 +1,5 @@
 VkontakteApi.configure do |config|
-  config.app_id       = '123'      # ID приложения
-  config.app_secret   = 'AbCdE654' # защищенный ключ
-  config.redirect_uri = 'http://vkontakte-on-rails.herokuapp.com/callback'
+  config.app_id       = '5231600'      # ID приложения
+  config.app_secret   = 'DVI2fW1V9vpq45hmuN1L' # защищенный ключ
+  config.redirect_uri = 'https://vk-music-downloader.herokuapp.com/callback'
 end
