a66247514386c1c4ef744457f53adf6ddc63bdde
yauhenininjia <thisislogiiiiin@gmail.com>
2016-01-19 13:12:53 +0300

    add puma, remove multidownloading

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,26 +5,32 @@ class SongsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @vk = VkontakteApi::Client.new current_user.token
-    @songs = @vk.audio.get
+    vk = VkontakteApi::Client.new current_user.token
+    @songs = vk.audio.get
   end
 
   def download
-    songs = params[:songs]
-    songs.each do |song|
-      song = JSON.parse song
-      uri = URI song['url']
-      filename = "#{song['filename']}.mp3"
-      file = nil
-      if File.exist? filename
-        file = File.open filename
-      else
-        file = File.new filename, 'wb'
-        file.write Net::HTTP.get uri
-      end
-      send_file file, disposition: 'attachment'
+    uri = URI params[:url]
+    filename = "app/assets/audio/#{params[:filename]}.mp3"
+    #songs = params[:songs]
+    #songs.map { |song| JSON.parse song }.each do |song|
+      #uri = URI song["url"]
+      #filename = "app/assets/audio/#{song['filename']}.mp3"
+      file = song_for_download(uri, filename)
+      send_file file.path, disposition: 'attachment'
+    #end
+  end
+
+  private
+
+  def song_for_download(uri, filename)
+    file = nil
+    if File.exist? filename
+      file = File.open filename
+    else
+      file = File.new filename, 'wb'
+      file.write Net::HTTP.get uri
     end
-    puts 'end'      
-      
+    file
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  post 'download' => 'songs#download'
+  get 'download' => 'songs#download'
   root 'songs#index'
 end
