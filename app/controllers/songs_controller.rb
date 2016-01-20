96c805adb21b90f5bbe9fc5614be42b91dc945d6
yauhenininjia <thisislogiiiiin@gmail.com>
2016-01-21 00:47:36 +0300

    ajax will paginate

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,15 @@
 require 'open-uri'
 require 'zip'
 require 'will_paginate/array'
+require 'action_view'
 
 class SongsController < ApplicationController
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  include ActionView::Helpers::NumberHelper
 
   after_action only: :download_zip do
-    logger.info "Start deleting zip #{@zipfile_path}"
+    logger.info "Start  deleting zip #{@zipfile_path}"
     File.delete(@zipfile_path)
     logger.info "Finish deleting zip #{@zipfile_path}"
   end
@@ -16,6 +18,10 @@ class SongsController < ApplicationController
     vk = VkontakteApi::Client.new current_user.token
     @songs = vk.audio.get
     @songs = @songs.paginate(page: params[:page], per_page: 10)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def download_zip
@@ -30,7 +36,7 @@ class SongsController < ApplicationController
     end
     create_zip(@zipfile_path, @files)
     
-    logger.info "Start sending #{@zipfile_path} to #{current_user.first_name} #{current_user.last_name}"
+    logger.info "Start  sending #{@zipfile_path} to #{current_user.first_name} #{current_user.last_name}"
     File.open(@zipfile_path, 'r') { |f| send_data f.read, filename: 'vk_audio.zip' }
     logger.info "Finish sending #{@zipfile_path} to #{current_user.first_name} #{current_user.last_name}"
   end
@@ -39,8 +45,8 @@ class SongsController < ApplicationController
     uri = URI params[:url]
     filename = mp3_filename(params[:filename])
     song_tmp_file = download_song_locally(params[:url], filename)
-    logger.info "Start sending #{filename} to #{current_user.first_name} #{current_user.last_name}"
-    #send_data open(params[:url]).read, filename: filename
+
+    logger.info "Start  sending #{filename} to #{current_user.first_name} #{current_user.last_name}"
     send_data song_tmp_file.read, filename: filename
     logger.info "Finish sending #{filename} to #{current_user.first_name} #{current_user.last_name}"
   end
@@ -48,13 +54,12 @@ class SongsController < ApplicationController
   private
 
   def create_zip(zipfile_path, files)
-    logger.info "Start creating zip in #{zipfile_path}"
+    logger.info "Start  creating zip in #{zipfile_path}"
 
     Zip::File.open(zipfile_path, Zip::File::CREATE) do |zipfile|
       files.each do |file|
         name = file[:mp3_filename]
         uri = URI(file[:url])
-        #song_tmp_file = open(file[:url])
         song_tmp_file = download_song_locally(file[:url], name)
 
         zipfile.add(name, song_tmp_file)
@@ -68,9 +73,10 @@ class SongsController < ApplicationController
   end
 
   def download_song_locally(url, filename = 'noname')
-    logger.info "Start downloading #{filename} from #{url}"
+    logger.info "Start  downloading #{filename} from #{url.truncate(60)}"
+    start = Time.now
     tmp_file = open(url)
-    logger.info "Finish downloading #{filename} from #{url}"
+    logger.info "Finish downloading #{filename} from #{url.truncate(60)}. Total #{number_to_human_size(tmp_file.length)} in #{(Time.now - start).round(2)}s"
     tmp_file
   end
 end
